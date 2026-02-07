32678e727624f1f8251b3f2abfec6c047e206b45
Abhishek Dahal <abhishek.dahal@truenary.com>
2026-02-07 10:58:47 +0545

    remove backend from app

--- backend/src/shared/db/schema.ts
+++
@@ -1,5 +0,0 @@
-export * from "@/features/auth/auth.schema";
-export * from "@/features/categories/categories.schema";
-export * from "@/features/sources/sources.schema";
-export * from "@/features/transactions/transactions.schema";
-export * from "@/features/user/user.schema";
